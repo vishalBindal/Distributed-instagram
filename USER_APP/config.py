0c699023b62e88e2c714b0089ed5ed01870a5c67
Vishal Singh <tmibvishal@gmail.com>
2022-03-28 21:56:25 +0530

    upload pic

---
+++ USER_APP/config.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+UPLOAD_FOLDER = './user_data/uploads'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 2 * 1024 * 1024  # Allow 2 mb of max upload
+
+MASTER_IP = '192.168.0.0'  # TODO: Add Master Node IP
---
+++ USER_APP/utils.py
@@ -0,0 +1,55 @@
+import os
+from flask import flash, request, redirect, url_for, send_from_directory
+from werkzeug.utils import secure_filename
+from config import ALLOWED_EXTENSIONS, app
+
+
+def allowed_file(filename: str):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/upload_pic', methods=['GET', 'POST'])
+def upload_pic():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+            # Process File
+
+
+            return redirect(url_for('download_file', name=filename))
+    return '''
+    <!doctype html>
+    <title>Upload new Image</title>
+    <h1>Upload new Image</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
+
+
+@app.route('/uploads/<name>')
+def download_file(name):
+    return send_from_directory(app.config["UPLOAD_FOLDER"], name)
+
+
+app.add_url_rule(
+    "/user_data/<name>", endpoint="download_file", build_only=True
+)
+
+if __name__ == "__main__":
+    # if not first time then remove this
+    app.run(debug=True, port=5022)
